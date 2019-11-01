8ad407b2c85a6d5d28601a566a2cb08ef479d75b
dmarsden <daniel.marsden@sdl.usu.edu>
2019-11-01 16:43:53 -0600

    marching squares update

--- libs/marching_squares.js
+++ libs/marching_squares.js
@@ -4,226 +4,328 @@
  *
  * Javascript port of :
  * http://devblog.phillipspiess.com/2010/02/23/better-know-an-algorithm-1-marching-squares/
- * returns an Array of x and y positions defining the perimeter of a blob of non-transparent pixels on a canvas
+ * returns an Array of x and y positions defining the perimeter of a blob of
+ * non-transparent pixels on a canvas
  *
  */
-(function (window){
+(function(window) {
 
-    // recursive function
-    // no need to worry about stepping out of bounds
-    // since the image is padded
-    function removeConnected(src, start, width) {
-        var idx, topIdx, leftIdx, bottomIdx, rightIdx;
-        idx = start.w + start.h*width;
-        topIdx = start.w + (start.h-1)*width
-        bottomIdx = start.w + (start.h+1)*width
-        leftIdx = idx-1;
-        rightIdx = idx+1;
-        src[idx] = 0;
+  const MarchingSquaresOpt = {};
 
-        if (src[topIdx] !== 0) {
-            removeConnected(src, {w:start.w, h:start.h-1}, width);
+  function labelConnectedComponents(src, height, width) {
+    var count = 1;
+    var conflicts = [];
+    for (var row = 0; row < height; row++) {
+      console.log("row:" + row);
+      for (var col = 0; col < width; col++) {
+        var idx = col + row * width;
+        if (src[idx] === 255) {
+          var idxBehind = idx - 1;
+          var idxAbove = idx - width;
+          // connected
+          if (src[idxBehind] !== 0 && src[idxAbove] !== 0) {
+            if (src[idxBehind] === src[idxAbove]) {
+              src[idx] = src[idxBehind];
+            } else {
+              // conflict - remember them for later
+              if (src[idxBehind] < src[idxAbove]) {
+                src[idx] = src[idxBehind];
+                conflicts.push({a: src[idxBehind], b:src[idxAbove] });
+              } else {
+                src[idx] = src[idxAbove];
+                conflicts.push({a: src[idxAbove], b:src[idxBehind] });
+              }
+            }
+          } else if (src[idxBehind] !== 0) {
+            src[idx] = src[idxBehind];
+          } else if (src[idxAbove] !== 0) {
+            src[idx] = src[idxAbove];
+          } else {
+            // not connected - label
+            src[idx] = count;
+            count++;
+          }
         }
-
-        if (src[bottomIdx] !== 0) {
-            removeConnected(src, {w:start.w, h:start.h+1}, width);
-        }
-
-        if (src[rightIdx] !== 0) {
-            removeConnected(src, {w:start.w+1, h:start.h}, width);
-        }
-
-        if (src[leftIdx] !== 0) {
-            removeConnected(src, {w:start.w-1, h:start.h-1}, width);
-        }
-
-        return;
+      }
     }
 
-    const MarchingSquaresOpt = {};
-
-    // return an array of objects
-    MarchingSquaresOpt.getBlobOutlinePoints = function(source_array, width, height=0){
-        // Note: object should not be on the border of the array, since there is
-        //       no padding of 1 pixel to handle points which touch edges
-
-        if (source_array instanceof HTMLCanvasElement){
-          width = source_array.width;
-          height = source_array.height;
-          const data4 = source_array.getContext('2d').getImageData(0, 0, width, height).data,  // Uint8ClampedArray
-            len = width * height,
-            data = new Uint8Array(len);
-          for (let i = 0; i < len; ++i){
-              data[i] = data4[i << 2];
+    // re- label
+    // for (var row = 0; row < height; row++) {
+    //   console.log("re label row:" + row);
+    //   for (var col = 0; col < width; col++) {
+    //     var idx = col + row * width;
+    //     conflicts.forEach(function (c) {
+    //       if (src[idx] === c.b) {
+    //         src[idx] = c.a;
+    //       }
+    //     });
+    //   }
+    // }
+    console.log("re label rows");
+    var len = height * width;
+    for (var i = 0; i < len; i++){
+      conflicts.forEach(function (c) {
+          if (src[i] === c.b) {
+            src[i] = c.a;
           }
-          source_array = data;
-        } else if (0 == height){
-            height = (source_array.length / width)|0;
+      });
+    }
+    console.log("done re-labeling");
+    // TODO write to disk?
+  }
+
+  // return an array of objects
+  MarchingSquaresOpt.getBlobOutlinePoints = function(
+      source_array, width, height = 0) {
+    // Note: object should not be on the border of the array, since there is
+    //       no padding of 1 pixel to handle points which touch edges
+
+    if (source_array instanceof HTMLCanvasElement) {
+      width = source_array.width;
+      height = source_array.height;
+      const data4 = source_array.getContext('2d')
+                        .getImageData(0, 0, width, height)
+                        .data,  // Uint8ClampedArray
+          len = width * height,
+            data = new Uint8Array(len);
+      for (let i = 0; i < len; ++i) {
+        data[i] = data4[i << 2];
+      }
+      source_array = data;
+    } else if (0 == height) {
+      height = (source_array.length / width) | 0;
+    }
+
+    // label all the connected components
+    console.log("labeling components with height:" + height + " width:" + width);
+    labelConnectedComponents(source_array, height, width);
+
+    // find the starting point
+    let startingPoint = MarchingSquaresOpt.getFirstNonTransparentPixelTopDown(
+        source_array, width, height);
+    if (null === startingPoint) {
+      console.log(
+          '[Warning] Marching Squares could not find an object in the given array');
+      return [];
+    }
+
+    var rslt = [];
+    while (startingPoint !== null) {
+      // debug only
+      // console.log(`Object start point w: ${startingPoint.w}, h:
+      // ${startingPoint.h}`);
+      var newObj = MarchingSquaresOpt.walkPerimeter(
+          source_array, width, height, startingPoint.w, startingPoint.h);
+      rslt.push(newObj);
+
+      // clear the labeled object in the array
+      var idx = startingPoint.w + startingPoint.h * width;
+      var removeLabel = source_array[idx];
+
+      console.log("removing comp:" + removeLabel);
+      var len = height * width;
+      for (var i = 0; i < len; i++){
+        if (source_array[i] !== 0) {
+          console.log("component label:" + source_array[i]);
         }
-
-        // find the starting point
-        let startingPoint = MarchingSquaresOpt.getFirstNonTransparentPixelTopDown(source_array, width, height);
-        if (null === startingPoint){
-            console.log('[Warning] Marching Squares could not find an object in the given array');
-            return [];
+        if (source_array[i] === removeLabel) {
+          source_array[i] = 0;
         }
+      }
+      console.log("done removing comp");
 
-        var rslt = [];
-        while(startingPoint !== null) {
-            // debug only
-            // console.log(`Object start point w: ${startingPoint.w}, h: ${startingPoint.h}`);
-            var newObj = MarchingSquaresOpt.walkPerimeter(source_array, width, height, startingPoint.w, startingPoint.h);
-            rslt.push(newObj);
-            // clear the object in the array
-            removeConnected(source_array, startingPoint, width);
+      // removeConnected(source_array, idx, width);
 
-            // find a new start point
-            startingPoint = MarchingSquaresOpt.getFirstNonTransparentPixelTopDown(source_array, width, height);
+      // find a new start point
+      startingPoint = MarchingSquaresOpt.getFirstNonTransparentPixelTopDown(
+          source_array, width, height);
+    }
+
+    // return an array of objects containing a list of w and h positions
+    return rslt;
+  };
+
+  MarchingSquaresOpt.getFirstNonTransparentPixelTopDown = function(
+      source_array, width, height) {
+    let idx;
+    for (let h = 0 | 0; h < height; ++h) {
+      idx = (h * width) | 0;
+      for (let w = 0 | 0; w < width; ++w) {
+        if (source_array[idx] > 0) {
+          return {w: w, h: h};
         }
+        ++idx;
+      }
+    }
+    return null;
+  };
 
-        // return an array of objects containing a list of w and h positions
-        return rslt;
-    };
+  MarchingSquaresOpt.walkPerimeter = function(
+      source_array, width, height, start_w, start_h) {
 
-    MarchingSquaresOpt.getFirstNonTransparentPixelTopDown = function(source_array, width, height){
-        let idx;
-        for(let h = 0|0; h < height; ++h){
-            idx = (h * width)|0;
-            for(let w = 0|0; w < width; ++w){
-                if(source_array[idx] > 0){
-                    return {w : w, h : h};
-                }
-                ++idx;
-            }
-        }
-        return null;
-    };
+    width = width | 0;
+    height = height | 0;
 
-    MarchingSquaresOpt.walkPerimeter = function(source_array, width, height, start_w, start_h){
+    // Set up our return list
+    const point_list = [], up = 1 | 0, left = 2 | 0, down = 3 | 0,
+          right = 4 | 0, step_func = MarchingSquaresOpt.step;
 
-        width = width|0;
-        height = height|0;
-
-        // Set up our return list
-        const point_list = [],
-            up = 1|0, left = 2|0, down = 3|0, right = 4|0,
-            step_func = MarchingSquaresOpt.step;
-
-        let idx = 0|0,  // Note: initialize it with an integer, so the JS interpreter optimizes for this type.
+    let idx = 0 | 0,  // Note: initialize it with an integer, so the JS
+                      // interpreter optimizes for this type.
 
         // our current x and y positions, initialized
         // to the init values passed in
-            w = start_w,
-            h = start_h,
+        w = start_w, h = start_h,
 
         // the main while loop, continues stepping until
         // we return to our initial points
-            next_step;
-        do {
-            // evaluate our state, and set up our next direction
-            idx = (h - 1) * width + (w - 1);
-            next_step = step_func(idx, source_array, width);
+        next_step;
+    do {
+      // evaluate our state, and set up our next direction
+      idx = (h - 1) * width + (w - 1);
+      next_step = step_func(idx, source_array, width);
 
-            // if our current point is within our image
-            // add it to the list of points
-            if (w >= 0 && w < width && h >= 0 && h < height){
-                point_list.push(w - 1, h);
-            }
+      // if our current point is within our image
+      // add it to the list of points
+      if (w >= 0 && w < width && h >= 0 && h < height) {
+        point_list.push(w - 1, h);
+      }
 
-            switch (next_step){
-                case up:    --h; break;
-                case left:  --w; break;
-                case down:  ++h; break;
-                case right: ++w; break;
-                default:
-                    break;
-            }
+      switch (next_step) {
+        case up:
+          --h;
+          break;
+        case left:
+          --w;
+          break;
+        case down:
+          ++h;
+          break;
+        case right:
+          ++w;
+          break;
+        default:
+          break;
+      }
 
-        } while (w != start_w || h != start_h);
+    } while (w != start_w || h != start_h);
 
-        point_list.push(w, h);
+    point_list.push(w, h);
 
-        return point_list;
-    };
+    return point_list;
+  };
 
-    // determines and sets the state of the 4 pixels that
-    // represent our current state, and sets our current and
-    // previous directions
+  // determines and sets the state of the 4 pixels that
+  // represent our current state, and sets our current and
+  // previous directions
 
-    MarchingSquaresOpt.step = function(idx, source_array, width){
-        //console.log('Sakri.MarchingSquaresOpt.step()');
-        // Scan our 4 pixel area
-        //Sakri.imageData = Sakri.MarchingSquaresOpt.sourceContext.getImageData(x-1, y-1, 2, 2).data;
+  MarchingSquaresOpt.step = function(idx, source_array, width) {
+    // console.log('Sakri.MarchingSquaresOpt.step()');
+    // Scan our 4 pixel area
+    // Sakri.imageData =
+    // Sakri.MarchingSquaresOpt.sourceContext.getImageData(x-1, y-1, 2, 2).data;
 
-        const up_left = 0 < source_array[idx + 1],
-            up_right = 0 < source_array[idx + 2],
-            down_left = 0 < source_array[idx + width + 1],
-            down_right = 0 < source_array[idx + width + 2],
-            none = 0|0, up = 1|0, left = 2|0, down = 3|0, right = 4|0;
+    const up_left = 0 < source_array[idx + 1],
+          up_right = 0 < source_array[idx + 2],
+          down_left = 0 < source_array[idx + width + 1],
+          down_right = 0 < source_array[idx + width + 2], none = 0 | 0,
+          up = 1 | 0, left = 2 | 0, down = 3 | 0, right = 4 | 0;
 
-        // Determine which state we are in
-        let state = 0|0;
+    // Determine which state we are in
+    let state = 0 | 0;
 
-        if (up_left){state |= 1;}
-        if (up_right){state |= 2;}
-        if (down_left){state |= 4;}
-        if (down_right){state |= 8;}
+    if (up_left) {
+      state |= 1;
+    }
+    if (up_right) {
+      state |= 2;
+    }
+    if (down_left) {
+      state |= 4;
+    }
+    if (down_right) {
+      state |= 8;
+    }
 
-        // State now contains a number between 0 and 15
-        // representing our state.
-        // In binary, it looks like 0000-1111 (in binary)
+    // State now contains a number between 0 and 15
+    // representing our state.
+    // In binary, it looks like 0000-1111 (in binary)
 
-        // An example. Let's say the top two pixels are filled,
-        // and the bottom two are empty.
-        // Stepping through the if statements above with a state
-        // of 0b0000 initially produces:
-        // Upper Left == true ==>  0b0001
-        // Upper Right == true ==> 0b0011
-        // The others are false, so 0b0011 is our state
-        // (That's 3 in decimal.)
+    // An example. Let's say the top two pixels are filled,
+    // and the bottom two are empty.
+    // Stepping through the if statements above with a state
+    // of 0b0000 initially produces:
+    // Upper Left == true ==>  0b0001
+    // Upper Right == true ==> 0b0011
+    // The others are false, so 0b0011 is our state
+    // (That's 3 in decimal.)
 
-        // Looking at the chart above, we see that state
-        // corresponds to a move right, so in our switch statement
-        // below, we add a case for 3, and assign Right as the
-        // direction of the next step. We repeat this process
-        // for all 16 states.
+    // Looking at the chart above, we see that state
+    // corresponds to a move right, so in our switch statement
+    // below, we add a case for 3, and assign Right as the
+    // direction of the next step. We repeat this process
+    // for all 16 states.
 
-        // So we can use a switch statement to determine our
-        // next direction based on
-        switch (state){
-            case 1: MarchingSquaresOpt.next_step = up; break;
-            case 2: MarchingSquaresOpt.next_step = right; break;
-            case 3: MarchingSquaresOpt.next_step = right; break;
-            case 4: MarchingSquaresOpt.next_step = left; break;
-            case 5: MarchingSquaresOpt.next_step = up; break;
-            case 6:
-                if (MarchingSquaresOpt.next_step == up){  // info from previous_step
-                    MarchingSquaresOpt.next_step = left;
-                } else {
-                    MarchingSquaresOpt.next_step = right;
-                }
-                break;
-            case 7: MarchingSquaresOpt.next_step = right; break;
-            case 8: MarchingSquaresOpt.next_step = down; break;
-            case 9:
-                if (MarchingSquaresOpt.next_step == right){  // info from previous_step
-                    MarchingSquaresOpt.next_step = up;
-                } else {
-                    MarchingSquaresOpt.next_step = down;
-                }
-                break;
-            case 10: MarchingSquaresOpt.next_step = down; break;
-            case 11: MarchingSquaresOpt.next_step = down; break;
-            case 12: MarchingSquaresOpt.next_step = left; break;
-            case 13: MarchingSquaresOpt.next_step = up; break;
-            case 14: MarchingSquaresOpt.next_step = left; break;
-            default:
-                MarchingSquaresOpt.next_step = none;  // this should never happen
-                break;
+    // So we can use a switch statement to determine our
+    // next direction based on
+    switch (state) {
+      case 1:
+        MarchingSquaresOpt.next_step = up;
+        break;
+      case 2:
+        MarchingSquaresOpt.next_step = right;
+        break;
+      case 3:
+        MarchingSquaresOpt.next_step = right;
+        break;
+      case 4:
+        MarchingSquaresOpt.next_step = left;
+        break;
+      case 5:
+        MarchingSquaresOpt.next_step = up;
+        break;
+      case 6:
+        if (MarchingSquaresOpt.next_step == up) {  // info from previous_step
+          MarchingSquaresOpt.next_step = left;
+        } else {
+          MarchingSquaresOpt.next_step = right;
         }
-        return MarchingSquaresOpt.next_step;
-    };
+        break;
+      case 7:
+        MarchingSquaresOpt.next_step = right;
+        break;
+      case 8:
+        MarchingSquaresOpt.next_step = down;
+        break;
+      case 9:
+        if (MarchingSquaresOpt.next_step == right) {  // info from previous_step
+          MarchingSquaresOpt.next_step = up;
+        } else {
+          MarchingSquaresOpt.next_step = down;
+        }
+        break;
+      case 10:
+        MarchingSquaresOpt.next_step = down;
+        break;
+      case 11:
+        MarchingSquaresOpt.next_step = down;
+        break;
+      case 12:
+        MarchingSquaresOpt.next_step = left;
+        break;
+      case 13:
+        MarchingSquaresOpt.next_step = up;
+        break;
+      case 14:
+        MarchingSquaresOpt.next_step = left;
+        break;
+      default:
+        MarchingSquaresOpt.next_step = none;  // this should never happen
+        break;
+    }
+    return MarchingSquaresOpt.next_step;
+  };
 
-    window.MarchingSquaresOpt = MarchingSquaresOpt;
+  window.MarchingSquaresOpt = MarchingSquaresOpt;
 
 }(window));
